061d52cb68322a26211ae30a366020bb81092222
a-maksimenko <a.maksimenko@dunice.net>
2022-05-30 12:09:53 +0300

    add create and update controller(base)

--- newsapp/app/controllers/news_controller.rb
+++ newsapp/app/controllers/news_controller.rb
@@ -3,4 +3,24 @@ class NewsController < ApplicationController
         @news = News.all.order(created_at: :asc)
         render json: @news
     end
+
+    def create
+        @news = News.create(news_params)    
+        if @news.save
+          render json: @news, status: :created, location: @news
+        else
+          render json: @news.errors, status: :unprocessable_entity
+        end
+      end    
+
+    def update    
+        if @news.update(news_params)      
+          @news.save
+          render json: @news
+        else
+          render json: @news.errors, status: :unprocessable_entity
+        end
+    end
+
+      
 end
--- newsapp/app/controllers/users_controller.rb
+++ newsapp/app/controllers/users_controller.rb
@@ -3,4 +3,22 @@ class UsersController < ApplicationController
         @users = User.all.order(created_at: :asc)
         render json: @users
     end
+
+    def create
+        @users = User.create(users_params)    
+        if @users.save
+          render json: @users, status: :created, location: @users
+        else
+          render json: @users.errors, status: :unprocessable_entity
+        end
+      end    
+
+    def update    
+        if @users.update(users_params)      
+          @users.save
+          render json: @users
+        else
+          render json: @users.errors, status: :unprocessable_entity
+        end
+    end
 end
